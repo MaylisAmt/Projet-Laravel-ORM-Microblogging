449eff2a7d4949c2901c042e4adfd07b08bb0d11
Maylis <maylis.ameto@gmail.com>
2023-08-01 16:58:20 +0200

    create and view posts

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,3 +20,4 @@ Route::get('/', function () {
 Route::get('/helloworld', function() {
     return view('helloworld');
 });
+Route::resource("posts", PostController::class);
